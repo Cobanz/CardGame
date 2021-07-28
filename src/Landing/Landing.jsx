7e6f589615937e649eeb4fd0c46e6d72270b003a
Ashley Knorr Gehring <ashleyknorr@gmail.com>
2021-07-28 16:55:33 -0500

    founders cards

--- src/Landing/Landing.jsx
+++ src/Landing/Landing.jsx
@@ -1,9 +1,141 @@
 import React from 'react'
+import Switch from '@material-ui/core/Switch';
+import Paper from '@material-ui/core/Paper';
+import Zoom from '@material-ui/core/Zoom';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import grant from './FB_IMG_1627059826106.jpg'
+
+
+const useStyles = makeStyles((theme) => ({
+    //   root: {
+    //     height: 380,
+    //   },
+      container: {
+        display: 'flex',
+      },
+      paper: {
+        margin: theme.spacing(1),
+      },
+      polygon: {
+        fill: theme.palette.common.white,
+        stroke: theme.palette.divider,
+        strokeWidth: 1,
+    
+        root: {
+            maxWidth: 555,
+          marginTop: '40px',
+          },
+          media: {
+            height: 120,
+          },
+      },
+    }));
 
 function Landing() {
+    const classes = useStyles();
+
     return (
         <div>
-            landing
+            <div class="float-container" style ={{
+    padding: '20px'}}>
+  <div class="float-child" style={{ width: '30%',
+    float: 'left',
+    padding: '20px'}}>
+  <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+    style={{height: 500}}
+          className={classes.media}
+          image="https://media-exp3.licdn.com/dms/image/C4E03AQELfcK4N0lwlg/profile-displayphoto-shrink_800_800/0/1593476412558?e=1628121600&v=beta&t=po62Qs-S0sqTqHL42DRqO16t01MNKq5lv2R0C13jEuM"
+          title="Photo of Founder"
+		/>
+        <CardContent >
+          <Typography gutterBottom variant="h5" component="h2">
+            Ashley Knorr Gehring
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+		  Ashley has 6 years experience as an engineer in the energy industry. Her previous focus has been design and operations, but now has shifted her focus to coding as a software developer. When she is not working to ensure CardWorks works seamlessly, she enjoys spending time with her family and traveling once again post-covid. 
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary" href="https://ashleykgehring.medium.com/" target="_blank">
+          Blog Posts
+        </Button>
+        <Button size="small" color="primary" href="https://www.linkedin.com/in/ashley-knorr-gehring-08302414/" target="_blank">
+          LinkedIn
+        </Button>
+      </CardActions>
+    </Card>
+  </div>
+  <div class="float-child"style={{ width: '30%',
+    float: 'left',
+    padding: '20px'}}>
+  <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+    style={{height: 440}}
+          className={classes.media}
+          image="https://media-exp1.licdn.com/dms/image/D5635AQF5awqRm8-lCw/profile-framedphoto-shrink_800_800/0/1626526501847?e=1627596000&v=beta&t=owtOtm3vPlqD54yxyTHCxW58RbNcEnTfHsh70VyzJlE"
+          title="Photo of Founder"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            David Henshaw
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          About David 
+		  </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary" href="https://medium.com/@davidhenshaw" target="_blank" >
+          Blog Posts
+        </Button>
+        <Button size="small" color="primary" href="https://www.linkedin.com/in/david-a-henshaw/" target="_blank" >
+          LinkedIn
+        </Button>
+      </CardActions>
+    </Card>
+  </div>
+  <div class="float-child"style={{ width: '30%',
+    float: 'left',
+    padding: '20px'}}>
+  <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+    style={{height: 440}}
+          className={classes.media}
+          image= {grant}
+          title="Founder Photo"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Grant Nichols
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          Grant is a military vet with 7 years experience in production. He loves playing Dungeons and Dragons and spending time with his son. He is coming up with ideas to expand CardWorks and is always up for a challenge.
+		  </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary" href="https://grantnichols-35018.medium.com/" target="_blank" > 
+		Blog Posts
+		</Button>
+        <Button size="small" color="primary" href="https://www.linkedin.com/in/grant-nichols-dev/" target="_blank" >
+          LinkedIn
+        </Button>
+      </CardActions>
+    </Card>
+  </div>
+</div>
         </div>
     )
 }
