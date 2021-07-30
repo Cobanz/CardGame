0b6b4912870a3b5f489870f05a856381d3a45821
Charles Grant Nichols <grant@inkdots.com>
2021-07-30 09:28:47 -0500

    staging game stuff

--- src/Game/Game.jsx
+++ src/Game/Game.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 function Game() {
     return (
-        <div>
+        <div className="game_container">
             Game
+            <div className="player_field">
+            player
+                <div className="player_deck">
+
+                </div>
+
+            </div>
+            <div className="computer_field">
+                computer
+                <div className="computer_deck">
+
+                </div>
+            </div>
+            <button>War</button>
         </div>
     )
 }
